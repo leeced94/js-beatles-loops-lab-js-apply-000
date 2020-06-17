b8072ed0670e0a27e8091c883559edafc83a4a8d
Cedric Lee <leeced@umich.edu>
2020-06-17 16:37:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 function theBeatlesPlay(array1, array2){
   let newArray = [];
   for ( let i = 0; i < array1.length; i++){
-    
+    newArray.push(array1[i] + "plays" + array2[i]);
   }
+  return newArray;
 }
